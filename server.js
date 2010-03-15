4bf7ff0ab1344988ab1333c4f49d9b7c4fbc42dc
Neeraj Singh <neerajdotname@gmail.com>
2010-03-15 15:34:20 -0400

    step2

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ PORT = 8002;
 HOST = '127.0.0.1';
 
 var sys = require('sys'),
+fs = require('fs');
 http = require('http');
 
 http.createServer(function(req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-	res.write('Hello World');
-	res.close();
+	fs.readFile('index.html', function(err, data) {
+		if (err) {
+			sys.puts('Error loading file index.html');
+		} else {
+			sys.puts('Loading file index.html');
+		}
 
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+			'Content-Length': data.length
+		});
+		res.write(data);
+		res.close();
+	});
 }).listen(PORT, HOST);
 
 sys.puts("Server at http://" + HOST + ':' + PORT.toString() + '/');
