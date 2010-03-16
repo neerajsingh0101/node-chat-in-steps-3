cbecd418a93381350e811922e5c278c0f38e2b8e
Neeraj Singh <neerajdotname@gmail.com>
2010-03-16 16:26:04 -0400

    step8

--- util.js
+++ util.js
@@ -21,7 +21,7 @@ util.createSession = function(nick) {
 	session = {
 		nick: nick,
 		id: Math.floor(Math.random() * 999999999999).toString()
-	}
+	};
 
 	util.sessions[session.id] = session;
 	return session;
@@ -73,6 +73,7 @@ util.staticHandler = function(filename) {
 util.get('/', util.staticHandler('index.html'));
 util.get('/jquery-1.4.2.js', util.staticHandler('jquery-1.4.2.js'));
 util.get('/client.js', util.staticHandler('client.js'));
+util.get('/style.css', util.staticHandler('style.css'));
 
 util.get('/join', function(req, res) {
 	var nick = qs.parse(url.parse(req.url).query).nick,
